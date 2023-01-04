c1672d23208c32c5a6e94f5d7e8b4b7ad8c33ec0
song-tiigger <applej07@naver.com>
2023-01-04 17:35:41 +0900

    mobileNave열기닫기 구현

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// 모바일 햄버거 메뉴 열기, 닫기
+function openMobileNav() {
+    document.getElementById("mobileNav").style.width = "100%"; 
+}
+
+function closeMobileNav() {
+    document.getElementById("mobileNav").style.width = "0"; 
+}
+
