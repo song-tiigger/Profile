5ef1a1cafa77bf915101f035821c25b8dc629a28
song-tiigger <applej07@naver.com>
2023-01-18 13:28:55 +0900

    스크롤 페이드인 구현중

--- js/index.js
+++ js/index.js
@@ -1,109 +1,109 @@
 // 제이쿼리
-$(function() {
-    // pc 메뉴 스크롤 이동
-    const gnbA = $('#gnb > li > a');
-    
-    gnbA.click(function() {
-        const target = $(this).attr('href');
-        $('html').animate({'scrollTop' : $(target).offset().top - 100});
-    })
+$(function () {
+  // pc 메뉴 스크롤 이동
+  const gnbA = $('#gnb > li > a');
 
-    // 모바일 메뉴 클릭하면 메뉴 창 닫힘
-    $(".mobile-nav > li > a").on("click", closeMobileNav
-    );
+  gnbA.click(function () {
+    const target = $(this).attr('href');
+    $('html').animate({ 'scrollTop': $(target).offset().top - 100 });
+  })
 
-    // 모바일 메뉴 클릭하면 스크롤로 이동 
-    const m_gnbA = $('.mobile-nav > li > a');
+  // 모바일 메뉴 클릭하면 메뉴 창 닫힘
+  $(".mobile-nav > li > a").on("click", closeMobileNav
+  );
 
-    m_gnbA.click(function() {
-        const target = $(this).attr('href');
-        $('html').animate({'scrollTop' : $(target).offset().top - 50});
+  // 모바일 메뉴 클릭하면 스크롤로 이동 
+  const m_gnbA = $('.mobile-nav > li > a');
 
-        // 모바일 메뉴 클릭하면 액티브 상태 적용
-        m_gnbA.removeClass('active');
-        $(this).addClass('active'); 
-    });
+  m_gnbA.click(function () {
+    const target = $(this).attr('href');
+    $('html').animate({ 'scrollTop': $(target).offset().top - 50 });
 
-    $(window).resize(function() {
-        if(window.innerWidth > 1200) {
-            $(".info-box").hide();
-        } else {
-            $(".info-box").show();
-        }
-    }).resize();
+    // 모바일 메뉴 클릭하면 액티브 상태 적용
+    m_gnbA.removeClass('active');
+    $(this).addClass('active');
+  });
 
-    // skill-circle에 마우스오버하면 info-box 나타나는 이벤트
-    $(".skill-circle div").on({
-        "mouseover" : function() {
-            var idx = $(".skill-circle div").index(this);
-            $("#skill .skill-info > div").eq(idx).fadeIn(400);
-        },
-        "mouseout" : function() {
-            var idx = $(".skill-circle div").index(this);
-            $("#skill .skill-info > div").eq(idx).stop().fadeOut(400);
-        }
-    });
-}); 
+  $(window).resize(function () {
+    if (window.innerWidth > 1200) {
+      $(".info-box").hide();
+    } else {
+      $(".info-box").show();
+    }
+  }).resize();
+
+  // skill-circle에 마우스오버하면 info-box 나타나는 이벤트
+  $(".skill-circle div").on({
+    "mouseover": function () {
+      var idx = $(".skill-circle div").index(this);
+      $("#skill .skill-info > div").eq(idx).fadeIn(400);
+    },
+    "mouseout": function () {
+      var idx = $(".skill-circle div").index(this);
+      $("#skill .skill-info > div").eq(idx).stop().fadeOut(400);
+    }
+  });
+});
 
 // 자바스크립트
-window.onload = function() {
-    // 자동 실행 함수 
-    let autoAnimaton = setInterval(() => {
-        autoFlip();
-        // movingCloud();
-    }, 1200);
-    
-    // header 텍스트 키워드 애니메이션
-    function autoFlip() {
-        document.querySelector('.prev').classList.remove('prev');
+window.onload = function () {
+  // 자동 실행 함수 
+  let autoAnimaton = setInterval(() => {
+    autoFlip();
+    // movingCloud();
+  }, 1200);
 
-        let current = document.querySelector('.current');
-        current.classList.remove('current');
-        current.classList.add('prev');
+  // header 텍스트 키워드 애니메이션
+  function autoFlip() {
+    document.querySelector('.prev').classList.remove('prev');
 
-        let next = document.querySelector('.next');
-        if(next.nextElementSibling == null) {
-            document.querySelector('#textFlip .text-wrap div:first-child').classList.add('next');
-        } else {
-            next.nextElementSibling.classList.add('next');
-        }
-        next.classList.remove('next');
-        next.classList.add('current');
+    let current = document.querySelector('.current');
+    current.classList.remove('current');
+    current.classList.add('prev');
+
+    let next = document.querySelector('.next');
+    if (next.nextElementSibling == null) {
+      document.querySelector('#textFlip .text-wrap div:first-child').classList.add('next');
+    } else {
+      next.nextElementSibling.classList.add('next');
     }
+    next.classList.remove('next');
+    next.classList.add('current');
+  }
 
-    // top 버튼 스크롤 애니메이션
-    const $topbtn = document.querySelector('.topbtn');
-    $topbtn.onclick = e => {
-        e.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        })
-    }
+  // top 버튼 스크롤 애니메이션
+  const $topbtn = document.querySelector('.topbtn');
+  $topbtn.onclick = e => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
 } //window.onload
 
 // 모바일 햄버거 메뉴 열기, 닫기
 function openMobileNav() {
-    document.getElementById("mobileNav").style.width = "100%"; 
+  document.getElementById("mobileNav").style.width = "100%";
 }
 
 function closeMobileNav() {
-    document.getElementById("mobileNav").style.width = "0"; 
-    document.getElementById("mobileNav").animate({opacity: "0"}, 600);
+  document.getElementById("mobileNav").style.width = "0";
+  document.getElementById("mobileNav").animate({ opacity: "0" }, 600);
 }
 
 // pc화면 상단에 gnb fix되는 기능
 function fixedMenu() {
-    let curPos = document.documentElement.scrollTop;
-    // console.log(curPos);
-    if(curPos > 60) {
-        document.getElementById('gnb').classList.add('menu_top');
-    } else {
-        document.getElementById('gnb').classList.remove('menu_top');
-    }
+  let curPos = document.documentElement.scrollTop;
+  // console.log(curPos);
+  if (curPos > 60) {
+    document.getElementById('gnb').classList.add('menu_top');
+  } else {
+    document.getElementById('gnb').classList.remove('menu_top');
+  }
 }
-document.addEventListener('scroll', fixedMenu); 
-        
+document.addEventListener('scroll', fixedMenu);
+
 
 
 
@@ -123,7 +123,8 @@ document.addEventListener('scroll', fixedMenu);
 
 
 // 스크롤 내릴 때 내용 보이기
-window.addEventListener('scroll', function() {
-    let scPos = window.scrollY;
-    // console.log(scPos);
+window.addEventListener('scroll', function () {
+  let scPos = window.scrollY;
+  
+  
 })
